dce23aa830f08c8750f3de0650f4dbc969ee0a80
ImmaSoftware <immasoftware@mail.ru>
2014-11-04 21:46:56 +0300

    Initial commit

---
+++ src/ru/mahach/Main.java
@@ -0,0 +1,9 @@
+package ru.mahach;
+
+public class Main extends Two{
+    public static void main(String[] args) {
+      printText("Hello Worldi");
+    }
+
+
+}
---
+++ src/ru/mahach/Two.java
@@ -0,0 +1,7 @@
+package ru.mahach;
+
+public class Two {
+    static void printText(String text){
+        System.out.print(text);
+    }
+}
