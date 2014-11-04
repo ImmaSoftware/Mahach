c31da1463148e741c44a9b0d879ba1bc8ff23c0f
ImmaSoftware <immasoftware@mail.ru>
2014-11-04 22:16:26 +0300

    Добавил изменения

--- src/ru/mahach/Two.java
+++ src/ru/mahach/Two.java
@@ -3,7 +3,5 @@ package ru.mahach;
 public class Two {
     static void printText(String text){
         System.out.print(text = " My text");
-        System.out.print("=========");
-        System.out.print("hellow");
     }
 }
